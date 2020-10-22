07848cb561a5970947e4b0b62facc99b01c9b9fd
JustinSoeung <justinsoeung94@gmail.com>
2020-10-21 21:47:33 -0500

    Added two columns and ran a new migration to do it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_025657) do
+ActiveRecord::Schema.define(version: 2020_10_22_022718) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_10_21_025657) do
     t.decimal "total_gross"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end
