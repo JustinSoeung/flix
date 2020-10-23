8a9ca46d6050a77887dacdd42c0145609d190629
JustinSoeung <justinsoeung94@gmail.com>
2020-10-22 22:09:21 -0500

    Added custom helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,17 @@
 module MoviesHelper
+    def concise (movie)
+        truncate(movie.description, length: 40, separator: ' ')
+    end
+
+    def total_gross(movie)
+        if movie.flop?
+           "Flop!"
+        else
+            number_to_currency(movie.total_gross, precision:0)
+        end
+    end
+    
+    def year_of(movie)
+        movie.released_on.year
+    end
 end
