1656d9d1bffc796922ed626900a596b618ab2645
JustinSoeung <justinsoeung94@gmail.com>
2020-10-25 21:02:26 -0500

    added an edit form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "movies#index"
 
-  get "movies"   => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
+  get "movies"          => "movies#index"
+  get "movies/:id"      => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
 end
