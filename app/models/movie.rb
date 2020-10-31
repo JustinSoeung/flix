43a9fc51de986cd5bc6d96ed6beb6132def79cb8
JustinSoeung <justinsoeung94@gmail.com>
2020-10-30 22:04:44 -0500

    added some more custom query

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,20 @@ class Movie < ApplicationRecord
     def self.released
         where("released_on < ?", Time.now).order("released_on desc")
     end
+
     def flop?
         total_gross.blank? || total_gross < 225_000_000
+    end
+
+    def self.hits
+        where("total_gross >= ?", 300_000_000).order("total_gross desc")
+    end
+
+    def self.flops
+        where("total_gross < ?", 225_000_000).order(total_gross: :asc)
+    end
+
+    def self.recently_added
+        order("created_at desc").limit(3)
     end
 end
