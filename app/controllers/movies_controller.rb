35e260b36aadfbc1fa3ae20d4a8edb9bd8d1668c
JustinSoeung <justinsoeung94@gmail.com>
2020-11-02 21:38:36 -0600

    handled validations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,8 +28,11 @@ class MoviesController < ApplicationController
     def create
 
         @movie = Movie.new(movie_params)
-        @movie.save
-        redirect_to @movie
+        if @movie.save
+            redirect_to @movie
+        else
+            render :new
+        end
         
     end
 
