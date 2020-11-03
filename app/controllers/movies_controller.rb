936be1770e1824be5e37abec8582027314d87412
JustinSoeung <justinsoeung94@gmail.com>
2020-11-02 21:46:01 -0600

    edit validations added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,8 +17,11 @@ class MoviesController < ApplicationController
 
     def update
         @movie = Movie.find(params[:id])
-        @movie.update(movie_params)
-        redirect_to movie_path(@movie)
+        if @movie.update(movie_params)
+            redirect_to movie_path(@movie)
+        else
+            render :edit
+        end
     end
 
     def new
