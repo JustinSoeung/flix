acdbd6f75a86468ba401acfeb22c5d44d43b147c
JustinSoeung <justinsoeung94@gmail.com>
2020-11-08 20:19:21 -0600

    Started building the create review action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,5 +3,10 @@ class ReviewsController < ApplicationController
       @movie = Movie.find(params[:movie_id])
       @reviews = @movie.reviews
     end
+
+    def new
+        @movie = Movie.find(params[:movie_id])
+        @review = @movie.reviews.new
+    end
   end
   
