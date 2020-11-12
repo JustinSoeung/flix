4d9bd355c96deca0b64367b19b9dec5e4dca7079
JustinSoeung <justinsoeung94@gmail.com>
2020-11-11 22:45:22 -0600

    added a users resource with name, email, and password

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+
+  validates :email, format: { with: /\S+@\S+/ },
+                    uniqueness: {case_sensitive: false }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :users
   root "movies#index"
   get "flops" => "movies#flops"
   resources :movies do
