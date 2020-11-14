4b5ce596256ae3f1a81b10b77bb2f961bcca9f2d
JustinSoeung <justinsoeung94@gmail.com>
2020-11-13 23:06:23 -0600

    added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,10 @@ class User < ApplicationRecord
   validates :email, format: { with: /\S+@\S+/ },
                     uniqueness: {case_sensitive: false }
 
-  validates :password, length: {minimum: 10, allow_blank: true}        
+  validates :password, length: {minimum: 10, allow_blank: true}
+  
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
+  
 end
