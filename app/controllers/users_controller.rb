695476ac551c94c9daf5ccb21be417a9bf303035
JustinSoeung <justinsoeung94@gmail.com>
2020-11-14 14:33:41 -0600

    Added edit and delete buttons for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,25 @@ class UsersController < ApplicationController
         
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user, notice: "Account successfully updated!"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_url, alert: "Account successfully deleted!"   
+    end
+
 private
     def user_params
         params.require(:user).
