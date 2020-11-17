3a08a6bb3c174893972884c58c889c7f28d84c8d
JustinSoeung <justinsoeung94@gmail.com>
2020-11-16 21:03:31 -0600

    Added an edit account link and optimized the # of times current_user is called

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             redirect_to @user, notice: "Account successfully created!"
         else
             render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :email, format: { with: /\S+@\S+/ },
                     uniqueness: {case_sensitive: false }
 
-  validates :password, length: {minimum: 10, allow_blank: true}
+  validates :password, length: {minimum: 6, allow_blank: true}
 
   validates :username, presence:true,
                     format: { with: /\A[A-Z0-9]+\z/i },
