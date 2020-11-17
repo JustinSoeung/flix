1edb89c9584a69388917bbb21f71ed9bb272fd2d
JustinSoeung <justinsoeung94@gmail.com>
2020-11-16 20:17:57 -0600

    added user authentification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user, notice: "Welcome back, #{user.name}!"
+        else
+            flash.now[:alert] = "Incorrect email/password"
+            render :new
+        end
     end
 
     def destroy
