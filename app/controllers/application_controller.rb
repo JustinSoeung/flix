c0ecc63a747d094f0a98c63c226829444c4e030d
JustinSoeung <justinsoeung94@gmail.com>
2020-11-20 23:12:37 -0600

    Hid buttons from users that are not admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,35 @@
 class ApplicationController < ActionController::Base
 
-private
-    def require_signin
+    private
+    
+      def require_signin
         unless current_user
-            session[:intended_url] = request.url
-            redirect_to signin_url, alert: "Please sign in first!"
+          session[:intended_url] = request.url
+          redirect_to new_session_url, alert: "Please sign in first!"
         end
-    end
-
-    def current_user
-        # User.find(session[:user_id]) if session[:user_id]
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
-
-    def current_user?(user)
-
-        current_user == user
-        
-    end
-
-    helper_method :current_user?
+      end
     
-
-    helper_method :current_user
-
-    def require_admin
+      def require_admin
         unless current_user_admin?
-            redirect_to root_url, alert: "Unauthorized access"
+          redirect_to root_url, alert: "Unauthorized access!"
         end
-        
-    end
+      end
     
-    def current_user_admin?
-        current_user && current_user_admin?
-    end
+      def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
     
-
-end
+      helper_method :current_user
+    
+      def current_user?(user)
+        current_user == user
+      end
+    
+      helper_method :current_user?
+    
+      def current_user_admin?
+        current_user && current_user.admin?
+      end
+    
+      helper_method :current_user_admin?
+    end
