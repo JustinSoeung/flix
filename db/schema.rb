a0dcb392119798ec1326886f0d1146832e3dacc2
JustinSoeung <justinsoeung94@gmail.com>
2020-11-20 22:48:04 -0600

    Added admin to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_025926) do
+ActiveRecord::Schema.define(version: 2020_11_21_044656) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_11_15_025926) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "movies"
