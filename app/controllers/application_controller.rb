fc2e43a3242625c56e7f3d391b443daae2d18ed4
JustinSoeung <justinsoeung94@gmail.com>
2020-11-20 23:01:54 -0600

    added admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,16 @@ private
 
     helper_method :current_user
 
+    def require_admin
+        unless current_user_admin?
+            redirect_to root_url, alert: "Unauthorized access"
+        end
+        
+    end
+    
+    def current_user_admin?
+        current_user && current_user_admin?
+    end
+    
+
 end
