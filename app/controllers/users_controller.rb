9e6429cb804c58f15c16521bd61f6921fc9f2d52
JustinSoeung <justinsoeung94@gmail.com>
2020-11-25 21:45:05 -0600

    Added through for users and movies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @reviews = @user.reviews
+        @favorite_movies = @user.favorite_movies
     end
 
     def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,7 @@ class Movie < ApplicationRecord
 
     has_many :reviews, dependent: :destroy
     has_many :favorites, dependent: :destroy
+    has_many :fans, through: :favorites, source: :user
 
     validates :title, :released_on, :duration, presence: true
 
