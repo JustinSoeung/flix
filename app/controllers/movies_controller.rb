8fd5559dcb598e776cc8ed5baa43f906096b8895
JustinSoeung <justinsoeung94@gmail.com>
2020-11-29 16:53:41 -0600

    added checkboxes for genres

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,7 @@ class MoviesController < ApplicationController
     def show
         @movie = Movie.find(params[:id])
         @fans = @movie.fans
+        @genres = @movie.genres
         @critics = @movie.critics
         if current_user
             @favorite = current_user.favorites.find_by(movie_id: @movie.id)
@@ -56,7 +57,7 @@ class MoviesController < ApplicationController
 private
     def movie_params
         params.require(:movie).
-        permit(:title,:description,:rating,:released_on,:total_gross,:director,:duration,:image_file_name)
+        permit(:title,:description,:rating,:released_on,:total_gross,:director,:duration,:image_file_name, genre_ids: [])
   
     end
 end
