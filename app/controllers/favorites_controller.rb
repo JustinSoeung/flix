a617c2d8a28cd2fc9d425ca87dff7632845ef4fe
JustinSoeung <justinsoeung94@gmail.com>
2020-12-05 20:52:21 -0600

    fave unfave button issues

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,27 +1,22 @@
 class FavoritesController < ApplicationController
     before_action :require_signin
-    before_action :set_movie
-
+  
     def create
-        # @movie = Movie.find(params[:movie_id]) #so this will get the movie id since the route needs it
-        @movie.favorites.create!(user: current_user) #This creates the favorite in the favorite model with the current user's id
-        
-        redirect_to @movie
+      @movie = Movie.find(params[:movie_id])
+      @movie.favorites.create!(user: current_user)
+  
+      # or append to the through association
+      # @movie.fans << current_user
+  
+      redirect_to @movie
     end
-
+  
     def destroy
-        @favorite = current_user.favorites.find_by(params[:id])
-        @favorite.destroy
-
-        redirect_to @movie
+      favorite = current_user.favorites.find(params[:id])
+      favorite.destroy
+  
+      movie = Movie.find(params[:movie_id])
+      redirect_to movie
     end
-    
-
-private
-    def set_movie
-        @movie = Movie.find(params[:movie_id])
-        
-    end
-    
-    
-end
+  end
+  
--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,11 +1,10 @@
 module FavoritesHelper
-
-    def fav_or_unfav(movie, favorite)
-        if @favorite
-            button_to "♡ Unfavorite", movie_favorite_path(movie, favorite), method: :delete
-        else
-            button_to "♥️ Favorite" , movie_favorites_path(movie)   
-        end
+    def fave_or_unfave_button(movie, favorite)
+      if favorite
+        button_to "♡ Unfave", movie_favorite_path(movie, favorite), method: :delete
+      else
+        button_to "♥️ Fave", movie_favorites_path(movie)
+      end
     end
-    
-end
+  end
+  
